d8b5a2b94e86f516bcf36ddd2a1945855628b04d
koize <justinweng6@gmail.com>
2024-07-24 12:10:22 +0800

    add webrtc cmd

--- device/init_cam_rpi/ccp_spy_device.sh
+++ device/init_cam_rpi/ccp_spy_device.sh
@@ -1,2 +1,7 @@
-apt-get install motion -y
+#!/bin/bash
 
+curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc |  apt-key add -
+echo "deb https://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" | tee /etc/apt/sources.list.d/uv4l.list
+apt-get update && apt-get upgrade
+apt-get install uv4l-webrtc
+uv4l --external-driver --device-name=video0
