0239ee880cfedc0661f96e6315ef27832861001e
koize <justinweng6@gmail.com>
2024-08-07 13:02:54 +0800

    update mqtt install script + fix broker ip (change to localhost)

--- device/init_cam_rpi/mqtt_install.sh
+++ device/init_cam_rpi/mqtt_install.sh
@@ -4,4 +4,7 @@ apt install -y mosquitto mosquitto-clients
 systemctl enable mosquitto.service
 mosquitto -v
 pip install paho-mqtt
-pip install opencv-python
+pip install opencv-python-headless==4.6.0.66
+pip install numpy==1.26.1
+sudo apt install -y libatlas-base-dev
+sudo apt install -y libopenblas-dev
